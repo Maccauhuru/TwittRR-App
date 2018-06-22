7544b48dcdff3331dd7aae819cb2b2b4c973dc2b
Simbarashe Mupfukudzwa <simba3310@gmail.com>
2018-06-21 19:08:06 -0500

    first commit

---
+++ src/components/tweet/Tweet.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Tweet=()=>{
+return(
+    <div className ='tweet'>
+    <p>Tweets Here</p>
+    </div>
+)
+}
+
+export default Tweet;
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Tweet from './components/tweet/Tweet';
+import './index.css';
+//import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(<Tweet />, document.getElementById('root'));
+
