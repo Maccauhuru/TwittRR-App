b04cb773f71b065392763fdbc1bcb8d3ed1d54bc
Simbarashe Mupfukudzwa <simba3310@gmail.com>
2018-07-03 19:20:11 -0500

    new seed file to populate tweet

---
+++ src/components/seed/TestTweet.js
@@ -0,0 +1,13 @@
+var TestTweet = {
+    message: "Something about react that just makes it such an exciting framework to work with! ",
+    gravatar: '95fb7218f3c9abaa46d38467f2f23ef4',
+    author: {
+        handle: "maccauhuru",
+        name: "Maccauhuru"
+    },
+    likes: 2,
+    retweets: 0,
+    timestamp: "2018-06-30 21:24:37"
+};
+
+export default TestTweet;
