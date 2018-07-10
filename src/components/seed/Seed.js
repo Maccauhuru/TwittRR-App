d42eba7c85ad3b87b8d8d5f3ea52bbfabad19498
Simbarashe Mupfukudzwa <simba3310@gmail.com>
2018-07-10 17:08:59 -0500

    new Seed file

---
+++ src/components/seed/Seed.js
@@ -0,0 +1,13 @@
+var Seed = {
+    message: "Something about react that just makes it such an exciting framework to work with! ",
+    gravatar: '95fb7218f3c9abaa46d38467f2f23ef4',
+    author: {
+        handle: "maccauhuru",
+        name: "Maccauhuru"
+    },
+    likes: 55,
+    retweets: 2000,
+    timestamp: "2018-07-04 21:24:37"
+};
+
+export default Seed;
